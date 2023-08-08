2be779408290537236814df47fb55152eac28c7c
waelhammi <weal.hammi@gmail.com>
2023-08-08 09:49:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderTop from './component/HeaderTop.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div> 
+<HeaderTop/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/HeaderTop.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
+function HeaderTop(){
+
+    return(
+
+        <div>
+
+<h2 style={{textAlign : "center", color : "blue"}}>Hello wael </h2>
+
+<Carousel>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  
+        </div>
+    )
+}
+
+export default HeaderTop;
